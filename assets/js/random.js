ded5412272c2fb35e8919f4b6e016f46a8703b29
Alex Torrance <hello@alextorrance.co.uk>
2016-07-11 23:45:23 +0100

    Expand javascript to make it easier to work with

--- assets/js/random.js
+++ assets/js/random.js
@@ -1,11 +1,70 @@
-$(document).ready(function() {
-  var concrete = new Array("table", "ladder", "staircase", "slide", "teapot", "cat's cradle", "pipeline", "engine", "roadmap", "bicycle", "basket", "refrigerator", "chimney", "window", "cupboard", "path", "plateau", "cul de sac", "highway", "incubator", "landscape", "umbrella", "spiral" )
-  randno = concrete[Math.floor( Math.random() * concrete.length )];
-  $('#concrete').text( randno );
-});
+(function() {
 
-$(document).ready(function() {
-  var abstract = new Array("hope", "hopelessness", "disappointment", "engagement", "opportunity", "empathy", "confusion", "despair", "success", "growth", "clarity", "poverty", "awareness", "actualisation", "forgiveness", "freedom", "imagination", "motivation", "speculation", "innovation" )
-  randno = abstract[Math.floor( Math.random() * abstract.length )];
-  $('#abstract').text( randno );
-});
+  "use strict";
+
+  var concreteArray = [
+    "table",
+    "ladder",
+    "staircase",
+    "slide",
+    "teapot",
+    "cat's cradle",
+    "pipeline",
+    "engine",
+    "roadmap",
+    "bicycle",
+    "basket",
+    "refrigerator",
+    "chimney",
+    "window",
+    "cupboard",
+    "path",
+    "plateau",
+    "cul de sac",
+    "highway",
+    "incubator",
+    "landscape",
+    "umbrella",
+    "spiral"
+  ];
+
+  var abstractArray = [
+    "hope",
+    "hopelessness",
+    "disappointment",
+    "engagement",
+    "opportunity",
+    "empathy",
+    "confusion",
+    "despair",
+    "success",
+    "growth",
+    "clarity",
+    "poverty",
+    "awareness",
+    "actualisation",
+    "forgiveness",
+    "freedom",
+    "imagination",
+    "motivation",
+    "speculation",
+    "innovation"
+  ];
+
+  var getRandomArrayElement = function(array) {
+    return array[Math.floor( Math.random() * array.length )];
+  }
+
+  var concreteAbstract = function() {
+    var $concrete = $('#concrete'),
+        $abstract = $('#abstract'),
+        concreteNoun = getRandomArrayElement(concreteArray),
+        abstractNoun = getRandomArrayElement(abstractArray);
+
+    $concrete.text(concreteNoun);
+    $abstract.text(abstractNoun);
+  }
+
+  $(document).ready(function() { concreteAbstract(); });
+
+})();
