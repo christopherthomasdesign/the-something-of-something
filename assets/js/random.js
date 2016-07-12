7816debaeabee310241590b60cd2218f2932e152
Alex Torrance <hello@alextorrance.co.uk>
2016-07-12 22:21:06 +0100

    Show diagram based on concrete noun
    Use the Bing image search API to search for the concrete noun, then insert the first result as an image tag into the DOM.
    
    There’s no  error handling, but it should fail silently

--- assets/js/random.js
+++ assets/js/random.js
@@ -63,7 +63,30 @@
 
     $concrete.text(concreteNoun);
     $abstract.text(abstractNoun);
+
+    getDiagram(concreteNoun);
   }
+
+  var getDiagram = function(q) {
+    var apiKey = "f4b1087863e9496a980a39e48754e715";
+
+    var bingURL = "https://api.cognitive.microsoft.com/bing/v5.0/images/search"
+      + "?q="+encodeURIComponent(q)
+      + "&color=Monochrome"
+      + "&imageType=Line"
+      + "&count=1";
+
+    $.ajax({
+      url: bingURL,
+      headers: { "Ocp-Apim-Subscription-Key" : apiKey },
+      success: function(data, status, xhr) {
+        var img = '<img src="'+data.value[0].contentUrl+'">';
+        $('.container').append(img);
+      }
+    });
+  }
+
+
 
   $(document).ready(function() { concreteAbstract(); });
 
